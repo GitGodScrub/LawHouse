c65701c9e552231998ba80088f2d4de7a14b1cc9
Daniella Clemmensen <dani2610@live.dk>
2018-11-28 12:21:11 +0100

    Lable navn

--- LawHouse/CreateAdvokatTester.Designer.cs
+++ LawHouse/CreateAdvokatTester.Designer.cs
@@ -31,9 +31,9 @@
             this.btn_CreateAdvokat = new System.Windows.Forms.Button();
             this.btn_AddSpecialToAdvokat = new System.Windows.Forms.Button();
             this.txt_AdvokatNavn = new System.Windows.Forms.TextBox();
-            this.label_AdvokatNavn = new System.Windows.Forms.Label();
-            this.Label_AdvokatId = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_AdvokatNavn = new System.Windows.Forms.Label();
+            this.lbl_AdvokatId = new System.Windows.Forms.Label();
+            this.lbl_SpecialeNavn = new System.Windows.Forms.Label();
             this.txt_AdvokatId = new System.Windows.Forms.TextBox();
             this.txt_SpecialeNavn = new System.Windows.Forms.TextBox();
             this.Btn_Help = new System.Windows.Forms.Button();
@@ -52,7 +52,7 @@
             // 
             // btn_AddSpecialToAdvokat
             // 
-            this.btn_AddSpecialToAdvokat.Location = new System.Drawing.Point(19, 194);
+            this.btn_AddSpecialToAdvokat.Location = new System.Drawing.Point(12, 194);
             this.btn_AddSpecialToAdvokat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_AddSpecialToAdvokat.Name = "btn_AddSpecialToAdvokat";
             this.btn_AddSpecialToAdvokat.Size = new System.Drawing.Size(133, 76);
@@ -69,32 +69,32 @@
             this.txt_AdvokatNavn.Size = new System.Drawing.Size(100, 22);
             this.txt_AdvokatNavn.TabIndex = 2;
             // 
-            // label_AdvokatNavn
+            // lbl_AdvokatNavn
             // 
-            this.label_AdvokatNavn.AutoSize = true;
-            this.label_AdvokatNavn.Location = new System.Drawing.Point(171, 49);
-            this.label_AdvokatNavn.Name = "label_AdvokatNavn";
-            this.label_AdvokatNavn.Size = new System.Drawing.Size(100, 17);
-            this.label_AdvokatNavn.TabIndex = 3;
-            this.label_AdvokatNavn.Text = "Advokat Navn:";
+            this.lbl_AdvokatNavn.AutoSize = true;
+            this.lbl_AdvokatNavn.Location = new System.Drawing.Point(171, 49);
+            this.lbl_AdvokatNavn.Name = "lbl_AdvokatNavn";
+            this.lbl_AdvokatNavn.Size = new System.Drawing.Size(100, 17);
+            this.lbl_AdvokatNavn.TabIndex = 3;
+            this.lbl_AdvokatNavn.Text = "Advokat Navn:";
             // 
-            // Label_AdvokatId
+            // lbl_AdvokatId
             // 
-            this.Label_AdvokatId.AutoSize = true;
-            this.Label_AdvokatId.Location = new System.Drawing.Point(199, 194);
-            this.Label_AdvokatId.Name = "Label_AdvokatId";
-            this.Label_AdvokatId.Size = new System.Drawing.Size(78, 17);
-            this.Label_AdvokatId.TabIndex = 4;
-            this.Label_AdvokatId.Text = "Advokat Id:";
+            this.lbl_AdvokatId.AutoSize = true;
+            this.lbl_AdvokatId.Location = new System.Drawing.Point(199, 194);
+            this.lbl_AdvokatId.Name = "lbl_AdvokatId";
+            this.lbl_AdvokatId.Size = new System.Drawing.Size(78, 17);
+            this.lbl_AdvokatId.TabIndex = 4;
+            this.lbl_AdvokatId.Text = "Advokat Id:";
             // 
-            // label2
+            // lbl_SpecialeNavn
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(173, 235);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(103, 17);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Speciale Navn:";
+            this.lbl_SpecialeNavn.AutoSize = true;
+            this.lbl_SpecialeNavn.Location = new System.Drawing.Point(173, 235);
+            this.lbl_SpecialeNavn.Name = "lbl_SpecialeNavn";
+            this.lbl_SpecialeNavn.Size = new System.Drawing.Size(103, 17);
+            this.lbl_SpecialeNavn.TabIndex = 5;
+            this.lbl_SpecialeNavn.Text = "Speciale Navn:";
             // 
             // txt_AdvokatId
             // 
@@ -114,8 +114,8 @@
             // 
             // Btn_Help
             // 
-            this.Btn_Help.Location = new System.Drawing.Point(12, 129);
-            this.Btn_Help.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Btn_Help.Location = new System.Drawing.Point(12, 134);
+            this.Btn_Help.Margin = new System.Windows.Forms.Padding(4);
             this.Btn_Help.Name = "Btn_Help";
             this.Btn_Help.Size = new System.Drawing.Size(100, 28);
             this.Btn_Help.TabIndex = 8;
@@ -131,9 +131,9 @@
             this.Controls.Add(this.Btn_Help);
             this.Controls.Add(this.txt_SpecialeNavn);
             this.Controls.Add(this.txt_AdvokatId);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.Label_AdvokatId);
-            this.Controls.Add(this.label_AdvokatNavn);
+            this.Controls.Add(this.lbl_SpecialeNavn);
+            this.Controls.Add(this.lbl_AdvokatId);
+            this.Controls.Add(this.lbl_AdvokatNavn);
             this.Controls.Add(this.txt_AdvokatNavn);
             this.Controls.Add(this.btn_AddSpecialToAdvokat);
             this.Controls.Add(this.btn_CreateAdvokat);
@@ -151,9 +151,9 @@
         private System.Windows.Forms.Button btn_CreateAdvokat;
         private System.Windows.Forms.Button btn_AddSpecialToAdvokat;
         private System.Windows.Forms.TextBox txt_AdvokatNavn;
-        private System.Windows.Forms.Label label_AdvokatNavn;
-        private System.Windows.Forms.Label Label_AdvokatId;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_AdvokatNavn;
+        private System.Windows.Forms.Label lbl_AdvokatId;
+        private System.Windows.Forms.Label lbl_SpecialeNavn;
         private System.Windows.Forms.TextBox txt_AdvokatId;
         private System.Windows.Forms.TextBox txt_SpecialeNavn;
         private System.Windows.Forms.Button Btn_Help;
