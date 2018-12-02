8169ca29321fc8f507fd2c2f34b39db1ce13c7ca
Julius Drejer Nielsen Brandt <juli3969@edu.eal.dk>
2018-12-02 17:33:07 +0100

    Started on DummyDatabase

---
+++ Database/DummyDatabase.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class DummyDatabase : IDatabase
+    {
+        public void AddEfteruddannelseToAdvokat(string efteruddannelse, int advokatId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateAdvokat(Advokat advokat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateKlient(Klient klient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateSag(Sag sag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateYdelse(Ydelse ydelse)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Advokat> GetAllAdvokat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Advokat> GetAllAdvokatFromYdelse(int ydelsesTypeNr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Klient> GetAllKlient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ListItems> GetAllList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Sag> GetAllSag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<YdelseList> GetAllTjenesteydelse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Ydelse> GetAllYdelse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<YdelseType> GetAllYdelseType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAdvokat(Advokat advokat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateKlient(Klient klient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSag(Sag sag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateYdelse(Ydelse ydelse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
