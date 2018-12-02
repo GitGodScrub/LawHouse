05d5034f09c6a6dc4178cd2294534daec094a2bf
Julius Drejer Nielsen Brandt <juli3969@edu.eal.dk>
2018-12-02 16:47:56 +0100

    Preperations for DummyDatabase

--- Businesslogi/Controller.cs
+++ Businesslogi/Controller.cs
@@ -11,13 +11,18 @@ namespace BusinessLogic
 
     public class Controller
     {
+        private static IDatabase currentDatabaseInstance;
+        public Controller()
+        {
+            currentDatabaseInstance = DatabaseHandler.Instance();
+            //currentDatabaseInstance = DummyDatabase();
+        }
         //Eksempel: private IDatabase _myDataBase = BadDataBase();
         // BadDataBase : IDatabase
         // DummyDatabase : IDatabase
         //
         //.Instance() pga det er en singleton
         //Before:  private static DatabaseHandler currentDatabaseInstance = DatabaseHandler.Instance();
-        private static IDatabase currentDatabaseInstance = DatabaseHandler.Instance();
 
         public static void CreateAdvokat(string advokatNavn)
         {
