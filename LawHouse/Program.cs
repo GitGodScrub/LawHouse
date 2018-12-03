2c55afdbcbe25dd883c91cb4605bf958f4ddb215
GitGodScrub <juliusbrandt@gmail.com>
2018-12-03 11:13:06 +0100

    THE MASTER IS DEAD, ALL HAIL THE MASTER

--- LawHouse/Program.cs
+++ LawHouse/Program.cs
@@ -16,7 +16,8 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new New_case());
+            Application.Run(new Startside()); //Ny
+            // Application.Run(new New_case()); //gammel
         }
     }
 }
