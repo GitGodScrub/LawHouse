129dddc7da663cdea79d7c00c0319568be648b48
GitGodScrub <juliusbrandt@gmail.com>
2018-12-04 12:19:21 +0100

    Small change

--- DataClassLibrary/Advokat.cs
+++ DataClassLibrary/Advokat.cs
@@ -4,14 +4,13 @@
     {
         public int AdvokatId { get; set; }
         public string Navn { get; set; }
-        public string Efteruddannelse { get; set; }
-
         public Advokat() { }
-
         public Advokat(string navn)
         {
             Navn = navn;
         }
+
+        public string Efteruddannelse { get; set; }
         public Advokat(int advokatId, string navn)
         {
             AdvokatId = advokatId;
