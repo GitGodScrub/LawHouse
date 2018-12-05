67828ffe78ba7eb416edf01b26bb364bec292bde
GitGodScrub <juliusbrandt@gmail.com>
2018-12-05 09:23:37 +0100

    Startup changes

--- LawHouse/Program.cs
+++ LawHouse/Program.cs
@@ -12,12 +12,27 @@ namespace GUI
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Startside()); //Ny
-            //Application.Run(new New_case()); //gammel
+            switch (args[0].ToString())
+            {
+                case "Ny GUI":
+                    Application.Run(new Startside());
+                    break;
+                case "Gammel GUI":
+                    Application.Run(new New_case());
+                    break;
+                case "":
+                    DefaultRun();
+                    break;
+            }//command argument [0] er det første ord, efter kommandoen, før mellemrum nr 2
+        }
+
+        static void DefaultRun()
+        {
+            Application.Run(new Startside());
         }
     }
 }
